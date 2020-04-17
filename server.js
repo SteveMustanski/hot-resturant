6c10c7ce0d8db66980e840e73d64ea3c17ab0d5f
SteveMustanski <steve.mustanski@gmail.com>
2020-04-17 09:20:05 -0400

    base working server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hey World!')
+})
+
+const port = 3000;
+app.listen(port)
+console.log(`server listening on ${port}`);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
